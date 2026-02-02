9a8963abbe8efcf9df33e49a939a7bbdee8a71b2
Akshit Ramola <ramolaakshit1@gmail.com>
2026-02-02 08:26:20 +0530

    add day 9

---
+++ Front/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+ 
+  const [ notes, setNotes] = useState([ 
+    {
+      title: "test title",
+      description: "test description"
+    },
+    {
+      title: "test title",
+      description: "test description"
+    },
+    {
+      title: "test title",
+      description: "test description"
+    },
+    {
+      title: "test title",
+      description: "test description"
+    }
+
+  ])
+  
+
+  return (
+    <>
+      <div className="notes">
+        {
+          notes.map(note => {
+            return <div className="note">
+              <h1>title</h1>
+              <p>description</p>
+            </div>
+          })
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
